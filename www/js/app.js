61d9882769e9d8b81b30d41c565e5fe5315fc939
Victor Cotrim <cotrim149@gmail.com>
2016-02-18 10:11:58 -0200

    Adding functions of controller and input again .run function

--- www/js/app.js
+++ www/js/app.js
@@ -5,30 +5,53 @@
 // the 2nd parameter is an array of 'requires'
 // angular.module('starter', ['ionic'])
 angular.module('todo', ['ionic'])
-// defining TodoCtrl controller which is called in index.html, in body tag
-.controller('TodoCtrl', function($scope) {
-  $scope.tasks = [
-    { title: 'Collect coins' },
-    { title: 'Eat mushrooms' },
-    { title: 'Get high enough to grab the flag' },
-    { title: 'Find the Princess' }
-  ];
-});
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-// .run(function($ionicPlatform) {
-//   $ionicPlatform.ready(function() {
-//     if(window.cordova && window.cordova.plugins.Keyboard) {
-//       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-//       // for form inputs)
-//       cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-//
-//       // Don't remove this line unless you know what you are doing. It stops the viewport
-//       // from snapping when text inputs are focused. Ionic handles this internally for
-//       // a much nicer keyboard experience.
-//       cordova.plugins.Keyboard.disableScroll(true);
-//     }
-//     if(window.StatusBar) {
-//       StatusBar.styleDefault();
-//     }
-//   });
-// })
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+// defining TodoCtrl controller which is called in index.html, in body tag
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  // No need for testing data anymore
+  $scope.tasks = [];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
+});
